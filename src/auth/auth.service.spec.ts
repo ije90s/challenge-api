255f03ca508a5c4f243c96ccf0e318e566836814
ije90s <idgocm@gmail.com>
2025-12-05 20:57:16 +0900

    auth service tdd

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service'; 
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let dto: any;
+  let result: any;
+
+  // DI 시킨 provider는 페이크 함수 처리
+  const mockUserService = {
+   findOne: jest.fn(),
+  };
+
+  const mockJwtService = {
+    sign: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UserService, useValue: mockUserService },
+        { provide: JwtService, useValue: mockJwtService },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('signIn', () => {
+    it('jwt 토큰 성공', async () => {
+      dto = { userId: 1, email: 'test@gmail.com', password: '1234' };
+
+      mockUserService.findOne.mockResolvedValue(dto); // 응답 주입
+      mockJwtService.sign.mockReturnValue('mock-token');
+
+      const result = await service.signIn({ email: 'test@gmail.com', password: '1234' });
+      expect(mockUserService.findOne).toHaveBeenCalledWith('test@gmail.com');
+      expect(mockJwtService.sign).toHaveBeenCalledWith({ email: dto.email, sub: dto.userId });
+      expect(result).toEqual({ access_token: 'mock-token' });
+    });
+
+    it('계정이 존재하지 않는 경우', async () => {
+      mockUserService.findOne.mockResolvedValue(null);
+
+      await expect(
+        service.signIn({ email: 'test@gmail.com', password: '1234' }),
+      ).rejects.toThrow('존재하지 않은 계정입니다.');
+    });
+
+    it('비밀번호가 틀린 경우', async () => {
+      dto = { userId: 1, email: 'test@gmail.com', password: '1234' };
+      mockUserService.findOne.mockResolvedValue(dto); // 응답 주입
+
+      await expect(
+        service.signIn({email: 'test@gmail.com', password: '1235'})
+      ).rejects.toThrow("비밀번호가 잘못되었습니다.");
+    });
+  });
+
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
+import { UserService } from '../user/user.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import { JwtService } from '@nestjs/jwt';
 
@@ -10,9 +10,9 @@ export class AuthService {
         private readonly jwtService: JwtService
     ){}
 
-    signIn(dto: LoginUserDto){
+    async signIn(dto: LoginUserDto){
         const {email, password } = dto;
-        const user = this.userService.findOne(email);
+        const user = await this.userService.findOne(email);
         if(!user){
             throw new UnauthorizedException("존재하지 않은 계정입니다.");
         }
