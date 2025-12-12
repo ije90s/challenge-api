f55c677f36d2fd2735574fe7aa03788a4635a58f
ije90s <idgocm@gmail.com>
2025-12-12 17:05:13 +0900

    User 서비스 로직 TDD

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,39 +1,32 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm/repository/Repository';
+import { User } from './entity/user.entity';
 
 @Injectable()
 export class UserService {
-    private readonly users = [
-        {
-        userId: 1,
-        email: 'test@gmail.com',
-        password: '1234',
-        },
-        {
-        userId: 2,
-        email: 'test2@gmail.com',
-        password: 'guess',
-        },
-    ];
+    constructor(@InjectRepository(User) private userRepository: Repository<User>){}
 
-    async signUp(dto: CreateUserDto){
+    async signUp(dto: CreateUserDto): Promise<User>{
         const { email, password } = dto;
         
         // DB 연결 전 확인 
-        const user = this.findOne(email);
+        const user = await this.findOneBy(email);
         if(user){
-            throw new UnauthorizedException("해당 이메일은 존재합니다.");
+            throw new ConflictException("이미 존재하는 이메일입니다.");
         }
 
         //패스워드 해싱 처리
         const hasedPassword = await bcrypt.hash(password, 10);
 
-        return `new account : ${email}, ${hasedPassword}`;
+        const newUser = this.userRepository.create({email, password: hasedPassword });
+        
+        return await this.userRepository.save(newUser);
     }
 
-    findOne(email: string){
-        const user = this.users.find(u => u.email === email);
-        return user;
+    async findOneBy(email: string): Promise<User | null>{
+        return this.userRepository.findOneBy({ email });
     }
 }
