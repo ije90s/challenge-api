0bd2bd014a05300661566ff8f1557458d45a73c2
ije90s <idgocm@gmail.com>
2025-12-12 17:04:56 +0900

    User 엔티티 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,14 @@ import { forwardRef, Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
