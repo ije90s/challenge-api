fb2b6bb9407fe91f3d426e621825b43b5dc593e4
ije90s <idgocm@gmail.com>
2025-12-17 17:36:07 +0900

    Participation TDD 완료

--- src/participation/participation.service.spec.ts
+++ src/participation/participation.service.spec.ts
@@ -4,6 +4,8 @@ import { ChallengeService } from '../challenge/challenge.service';
 import { checkThePast } from '../common/util';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Participation } from './entity/participation.entity';
+import { CreateParticipationDto } from './dto/create-participation.dto';
+import { UpdateParticipationDto } from './dto/update-participation.dto';
 
 // 올바른 mock 경로
 jest.mock('../common/util', () => ({
@@ -57,6 +59,7 @@ describe('ParticipationService', () => {
         user: { id: 1 } as any,
         created_at: today,
         updated_at: today,
+        complete_date: null,
       },
       {
         id: 2,
@@ -67,7 +70,30 @@ describe('ParticipationService', () => {
         user: { id: 2 } as any,
         created_at: today,
         updated_at: today,
+        complete_date: null,
       },
+      {
+        id: 3,
+        score: 0,
+        challenge_count: 0,
+        status: 2,
+        challenge: { id: 1 } as any,
+        user: { id: 4 } as any,
+        created_at: today,
+        updated_at: today,
+        complete_date: null,
+      },
+      {
+        id: 4,
+        score: 1,
+        challenge_count: 0,
+        status: 1,
+        challenge: { id: 1 } as any,
+        user: { id: 5 } as any,
+        created_at: today,
+        updated_at: today,
+        complete_date: new Date(),
+      }
     ];
   });
 
@@ -108,6 +134,212 @@ describe('ParticipationService', () => {
   describe("create", () => {
 
     const challenge = {
+      challenge_id: 1,
+      type: 0, 
+      mininum_count: 1,
+      title: '테스트',
+      content: '테스트',
+      start_date: '2025-12-01',
+      end_date: '2025-12-31',
+      author: {id: 1} as any, 
+      created_at: today,
+      updated_at: today,
+      deleted_at: null,
+    };
+
+    let participation: Object;
+    let dto: CreateParticipationDto;
+
+    beforeEach(() => {
+      jest.clearAllMocks();                 
+      mockChallengeService.findOne.mockResolvedValue(challenge);
+      (checkThePast as jest.Mock).mockReturnValue(true);
+      participation = {
+        id: 3,
+        score: 0,
+        challenge_count: 0,
+        status: 0,
+        challenge: { id: 1 } as any,
+        user: { id: 3 } as any,
+        created_at: today,
+        updated_at: today,
+      }
+    });
+
+    it("참가 성공", async () => {
+      dto = { challenge_id: 1, };
+      mockParticipationService.findOne.mockResolvedValue(null);
+      mockParticipationService.create.mockReturnValue(participation);
+      mockParticipationService.save.mockResolvedValue(participation);
+
+      result = await service.create(3, dto);
+      expect(mockChallengeService.findOne).toHaveBeenCalledWith(1);
+      expect(checkThePast).toHaveBeenCalledWith(challenge.end_date);
+      expect(mockParticipationService.findOne).toHaveBeenCalledWith({
+        where: {
+          challenge: { id: 1 },
+          user: { id: 3 },
+        },
+      });
+      expect(mockParticipationService.create).toHaveBeenCalledWith({
+        challenge: { id: dto.challenge_id },
+        user: { id: 3 },
+      });
+      expect(result.id).toBe(3);
+    });
+
+    it("챌린지가 존재하지 않은 경우", async () => {
+      mockChallengeService.findOne.mockResolvedValue(null);
+
+      await expect(service.create(3, {challenge_id: 2})).rejects.toThrow("챌린지가 존재하지 않습니다.");
+    });
+
+    it("기간이 지난 경우", async () => {
+      (checkThePast as jest.Mock).mockReturnValue(false);
+
+      await expect(service.create(4, {challenge_id: 1})).rejects.toThrow("기간이 지났습니다.");
+    });
+
+    it("이미 참가 중인 경우", async () => {
+      mockParticipationService.findOne.mockResolvedValue(participations[0]);
+      await expect(service.create(1, {challenge_id: 1})).rejects.toThrow("이미 참가중입니다.");
+    });
+  });
+
+  describe("update", () => {
+    const challenge = {
+      challengeId: 1,
+      type: 0, 
+      mininum_count: 1,
+      title: '테스트',
+      content: '테스트',
+      start_date: '2025-12-01',
+      end_date: '2025-12-31'
+    };
+
+    let participation: Participation;
+    let dto: UpdateParticipationDto;
+
+    beforeEach(() => {
+      mockChallengeService.findOne.mockResolvedValue(challenge);
+      (checkThePast as jest.Mock).mockReturnValue(true);
+    });
+
+    it("기록 업데이트 성공 - 값이 없는 경우", async () => {
+      dto = {challenge_id: 1 };
+      participation = {
+        ...participations[1],
+        updated_at: today,
+      }
+      mockParticipationService.findOne.mockResolvedValue(participations[1]);
+      mockParticipationService.save.mockResolvedValue(participation);
+
+      result = await service.update(2, dto);
+      expect(mockChallengeService.findOne).toHaveBeenCalledWith(1);
+      expect(checkThePast).toHaveBeenCalledWith(challenge.end_date);
+      expect(mockParticipationService.findOne).toHaveBeenCalledWith({
+        where: {
+          challenge: { id: 1 },
+          user: { id: 2 },
+        },
+      });
+      expect(mockParticipationService.save).toHaveBeenCalledWith(participation);
+      expect(result.score).toBe(0);
+      expect(result.complete_date).toBeNull();
+    });
+    
+    it("기록 업데이트 성공 - 값이 있는 경우", async () => {
+      dto = {challenge_id: 1, score: 1 };
+      participation = {
+        ...participations[0],
+        complete_date: new Date(),
+        score: 2,
+        status: 1,
+        updated_at: today,
+      }
+      mockParticipationService.findOne.mockResolvedValue(participations[0]);
+      mockParticipationService.save.mockResolvedValue(participation);
+
+      result = await service.update(1, dto);
+      expect(mockChallengeService.findOne).toHaveBeenCalledWith(1);
+      expect(checkThePast).toHaveBeenCalledWith(challenge.end_date);
+      expect(mockParticipationService.findOne).toHaveBeenCalledWith({
+        where: {
+          challenge: { id: 1 },
+          user: { id: 1 },
+        },
+      });
+      expect(mockParticipationService.save).toHaveBeenCalledWith(participation);
+      expect(result.score).toBe(2);
+      expect(result.complete_date).not.toBeNull();
+    });
+
+    it('참가하지 않은 경우', async () => {
+      mockParticipationService.findOne.mockResolvedValue(null);
+      await expect(service.update(4, {challenge_id: 1})).rejects.toThrow("참가하지 않았습니다.");
+    });
+
+    it('챌린지 포기 상태', async () => {
+      mockParticipationService.findOne.mockResolvedValue(participations[2]);
+      await expect(service.update(4, {challenge_id: 1})).rejects.toThrow("챌린지 포기 상태입니다.");
+    });
+  });
+
+  describe("updateStatus", () => {
+    let dto: UpdateParticipationDto;
+    let participation: Participation;
+
+    it("챌린지 포기 성공", async () => {
+      dto = {challenge_id: 1};
+      participation = {
+        ...participations[0],
+        status: 2,
+      }
+      mockParticipationService.findOne.mockResolvedValue(participations[0]);
+      mockParticipationService.save.mockResolvedValue(participation);
+      result = await service.updateStatus(1, dto);
+      expect(mockParticipationService.findOne).toHaveBeenCalledWith({
+        where: {
+          challenge: { id: 1 },
+          user: { id: 1 },
+        },
+      });
+      expect(mockParticipationService.save).toHaveBeenCalledWith(participation);
+      expect(result.status).toBe(2);
+    });
+
+    it("챌린지 포기 취소", async () => {
+      dto = {challenge_id: 1};
+      participation = {
+        ...participations[2],
+        status: 0,
+      };
+      mockParticipationService.findOne.mockResolvedValue(participations[2]);
+      mockParticipationService.save.mockResolvedValue(participation);
+      result = await service.updateStatus(4, dto);
+      expect(mockParticipationService.findOne).toHaveBeenCalledWith({
+        where: {
+          challenge: { id: 1 },
+          user: { id: 4 },
+        },
+      });
+      expect(mockParticipationService.save).toHaveBeenCalledWith(participation);
+      expect(result.status).toBe(0);
+    });
+
+    it("참가하지 않은 경우", async () => {
+      mockParticipationService.findOne.mockResolvedValue(null);
+      await expect(service.updateStatus(3, {challenge_id: 1})).rejects.toThrow("참가하지 않았습니다.");
+    });
+
+    it("챌린지 완료하는 경우", async () => {
+      mockParticipationService.findOne.mockResolvedValue(participations[3]);
+      await expect(service.updateStatus(5, {challenge_id: 1})).rejects.toThrow("이미 챌린지 완료했습니다.");
+    });
+  });
+
+  describe("getChallengeRank", () => {
+    const challenge = {
       challengeId: 1,
       type: 0, 
       mininum_count: 1,
@@ -118,149 +350,56 @@ describe('ParticipationService', () => {
     };
 
     beforeEach(() => {
-      jest.clearAllMocks();                 
       mockChallengeService.findOne.mockResolvedValue(challenge);
-      (checkThePast as jest.Mock).mockReturnValue(true);
+    });
+    it("점수순으로 리스트 가져오기", async () => {
+      const challengeId = 1;
+      const orderField = 'p.score';
+      const orderdParticipations = participations.sort((a,b) => {
+        return b.score - a.score;
+      });
+      mockParticipationService.findOne.mockResolvedValue(participations[0]);
+      mockQueryBuilder.getMany.mockResolvedValue(orderdParticipations);
+
+      result = await service.getChallengeRank(challengeId, 1);
+      expect(mockParticipationService.createQueryBuilder).toHaveBeenCalledWith('p');
+      expect(mockQueryBuilder.where).toHaveBeenCalledWith('p.challenge_id = :challengeId', { challengeId })
+      expect(mockQueryBuilder.orderBy).toHaveBeenCalledWith(orderField, 'DESC')
+      expect(mockQueryBuilder.addOrderBy).toHaveBeenCalledWith('p.created_at', 'DESC')
+      expect(mockQueryBuilder.getMany).toHaveBeenCalled();
+      expect(result).toEqual(orderdParticipations);
     });
 
-    it("참가 성공 - 값이 없는 경우", async () => {
-      result = await service.create(1,4,{});
-
-      expect(mockChallengeService.findOne).toHaveBeenCalledWith(1);
-      expect(checkThePast).toHaveBeenCalledWith(challenge.end_date);
-
-      expect(result.participation_id).toBe(4);
-      expect(result.status).toBe(0);
-      expect(result.challenge_count).toBe(0);
-      expect(result.score).toBe(0);
-    });
-
-    it("참가 성공 - 값이 있는 경우", async () => {
-      result = await service.create(1,4,{score: 0, challenge_count: 0});
-
-      expect(result.participation_id).toBe(4);
-      expect(result.status).toBe(0);
-    });
-
-    it("챌린지가 존재하지 않은 경우", async () => {
-      mockChallengeService.findOne.mockResolvedValue(null);
-
-      await expect(service.create(4, 4, {}))
-        .rejects.toThrow("챌린지가 존재하지 않습니다.");
-    });
-
-    it("기간이 지난 경우", async () => {
-      (checkThePast as jest.Mock).mockReturnValue(false);
-
-      await expect(service.create(1, 4, {}))
-        .rejects.toThrow("기간이 지났습니다.");
-    });
-
-    it("이미 참가 중인 경우", async () => {
-      await expect(service.create(1, 1, {}))
-        .rejects.toThrow("이미 참가중입니다.");
+    it("참가하지 않은 경우", async () => {
+      mockParticipationService.findOne.mockResolvedValue(null);
+      await expect(service.getChallengeRank(1, 3)).rejects.toThrow("참가하지 않았습니다.");
     });
   });
 
-  // // -----------------------------
-  // // update
-  // // -----------------------------
-  // describe("update", () => {
-  //   const challenge = {
-  //     challengeId: 1,
-  //     type: 0, 
-  //     mininum_count: 1,
-  //     title: '테스트',
-  //     content: '테스트',
-  //     start_date: '2025-12-01',
-  //     end_date: '2025-12-31'
-  //   };
+  describe("getMyChallenge", () => {
+    it("챌린지 참가한 경우", async () => {
+      mockParticipationService.find.mockResolvedValue(participations[0]);
+      result = await service.getMyChallenge(1);
+      expect(mockParticipationService.find).toHaveBeenCalledWith({
+        where: {
+          user: { id: 1 },
+        },
+        order: { created_at: 'DESC' },
+      });
+      expect(result).toBeTruthy();
+    });
 
-  //   beforeEach(() => {
-  //     mockChallengeService.findOne.mockResolvedValue(challenge);
-  //     (checkThePast as jest.Mock).mockReturnValue(true);
-  //   });
-
-  //   it("기록 업데이트 성공 - 값이 없는 경우", async () => {
-  //     result = await service.update(1, 1, {});
-
-  //     const pickOne = result.filter(item => item.challenge_id === 1 && item.user_id === 1);
-  //     expect(pickOne[0].score).toBe(0);
-  //     expect(pickOne[0].challenge_count).toBe(0);
-  //     expect(pickOne[0].status).toBe(0);
-
-  //     expect(mockChallengeService.findOne).toHaveBeenCalledWith(1);
-  //     expect(checkThePast).toHaveBeenCalledWith(challenge.end_date);
-  //   });
-    
-  //   it("기록 업데이트 성공 - 값이 있는 경우", async () => {
-  //     result = await service.update(1, 1, {score: 1});
-
-  //     const pickOne = result.filter(item => item.challenge_id === 1 && item.user_id === 1);
-  //     expect(pickOne[0].score).toBe(1);
-  //   });
-
-  //   it('참가하지 않은 경우', async () => {
-  //     await expect(service.update(1, 4, {}))
-  //       .rejects.toThrow("참가하지 않았습니다.");
-  //   });
-
-  //   it('챌린지 포기 상태', async () => {
-  //     await expect(service.update(1, 3, {}))
-  //       .rejects.toThrow("챌린지 포기 상태입니다.");
-  //   });
-  // });
-
-  // // -----------------------------
-  // // updateStatus
-  // // -----------------------------
-  // describe("updateStatus", () => {
-  //   it("챌린지 포기 성공", async () => {
-  //     result = await service.updateStatus(1,1);
-  //     const pickOne = result.filter(item => item.challenge_id === 1 && item.user_id === 1);
-  //     expect(pickOne[0].status).toBe(2);
-  //   });
-
-  //   it("챌린지 포기 취소", async () => {
-  //     result = await service.updateStatus(1, 3);
-  //     const pickOne = result.filter(item => item.challenge_id === 1 && item.user_id === 3);
-  //     expect(pickOne[0].status).toBe(0);
-  //   });
-
-  //   it("참가하지 않은 경우", async () => {
-  //     await expect(service.updateStatus(1, 4))
-  //       .rejects.toThrow("참가하지 않았습니다.");
-  //   });
-  // });
-
-  // // -----------------------------
-  // // getChallengeRank
-  // // -----------------------------
-  // describe("getChallengeRank", () => {
-  //   it("리스트 가져오기", async () => {
-  //     result = await service.getChallengeRank(1, 1);
-  //     expect(result.length).toBe(3);
-  //   });
-
-  //   it("참가하지 않은 경우", async () => {
-  //     await expect(service.updateStatus(1, 4))
-  //       .rejects.toThrow("참가하지 않았습니다.");
-  //   });
-  // });
-
-  // // -----------------------------
-  // // getMyChallenge
-  // // -----------------------------
-  // describe("getMyChallenge", () => {
-  //   it("챌린지 참가한 경우", () => {
-  //     result = service.getMyChallenge(1);
-  //     expect(result).toBeDefined();
-  //   });
-
-  //   it("없는 경우", () => {
-  //     result = service.getMyChallenge(4);
-  //     expect(result).toBeUndefined();
-  //   });
-  // });
+    it("없는 경우", async () => {
+      mockParticipationService.find.mockResolvedValue([]);
+      result = await service.getMyChallenge(9);
+      expect(mockParticipationService.find).toHaveBeenCalledWith({
+        where: {
+          user: { id: 9 },
+        },
+        order: { created_at: 'DESC' },
+      });
+      expect(result.length).toBe(0);
+    });
+  });
 
 });
