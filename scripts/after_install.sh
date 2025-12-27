f8d67dd7b549743cd3cf753030ff8de143a7facd
ije90s <idgocm@gmail.com>
2025-12-27 23:01:09 +0900

    파라미터 스토어를 이용하여 .env 파일 생성

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 set -e
 
-APP_DIR=/home/ubuntu/app
-ENV_SRC=/home/ubuntu/.env.prod
-ENV_DST=$APP_DIR/.env
-ENV_BAK=$APP_DIR/.env.bak
-
-if [ ! -f "$ENV_SRC" ]; then
-  echo ".env.prod not found"
-  exit 1
-fi
-
-if [ -f "$ENV_DST" ]; then 
-  cp "$ENV_DST" "$ENV_BAK"
-fi
-
-cp "$ENV_SRC" "$ENV_DST"
-chmod 600 "$ENV_DST"
-
 npm ci
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,29 @@
 set -e
 
 mkdir -p /home/ubuntu/app
+
+PARAM_PATH=/challenge-api/prod
+
+ENV_FILE=/home/ubuntu/app/.env
+
+echo "--------------- .env 파일 생성 시작 -----------------" 
+
+# .env 초기화
+> $ENV_FILE
+
+# 파라미터 값 가져오기
+PARAMS=$(aws ssm get-parameters-by-path \
+  --path "$PARAM_PATH" \
+  --with-decryption \
+  --query "Parameters[*].[Name,Value]" \
+  --output text)
+
+# 반복문으로 .env 파일 값 셋팅
+while read -r name value; do
+  key=$(basename "$name")
+  echo "$key=$value" >> $ENV_FILE
+done <<< "$PARAMS"
+
+chmod 600 $ENV_FILE
+
+echo "--------------- .env 파일 생성 끝 -----------------" 
