6e2d807f55d8f5f44b29843b6a498e70b12726cc
ije90s <idgocm@gmail.com>
2025-12-28 21:43:30 +0900

    헬스 체크 실패 테스트

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,5 +4,5 @@ set -e
 echo "--------------- 배포 시작 -----------------" 
 cd /home/ubuntu/app 
 
-pm2 startOrReload ecosystem.config.js --env production
+# pm2 startOrReload ecosystem.config.js --env production
 echo "--------------- 배포 끝 -----------------"
